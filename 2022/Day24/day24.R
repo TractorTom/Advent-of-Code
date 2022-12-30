71881aa91776307886e74aac9af566c3fa79e14e
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2022-12-30 13:05:38 +0100

    first optimisation day 24

--- 2022/Day24/day24.R
+++ 2022/Day24/day24.R
@@ -1,6 +1,10 @@
 # Titre : day24.R
 # Auteur : Tanguy
 
+## Pb à résoudre :
+#   - cerains noeux sont répétés lors de leur évaluation (méthode 1 et 2)
+#   - la seconde méthode sevrait être plus rapide !
+
 ##############
 ##  DAY 24  ##
 ##############
@@ -11,11 +15,14 @@ map_valley <- readLines("./2022/Day24/valley_map.txt")
 map_valley_example <- readLines("./2022/Day24/valley_map_example.txt")
 map_valley_example2 <- readLines("./2022/Day24/valley_map_example2.txt")
 
-
 # Déclaration fonction ---------------------------------------------------------
 
 ppcm <- function(x, y) {
     return(x * y %/% schoolmath::gcd(x, y))
+}
+
+modulo_vectorise <- function(indexs, pos, all_step) {
+    (function(steps) ((function(step) (pos + step - indexs) %% length(indexs) == 0) |> Vectorize())(steps))(all_step)
 }
 
 get_blizzard_position <- function(data_valley) {
@@ -44,43 +51,42 @@ get_blizzard_position <- function(data_valley) {
     return(blizzard_position)
 }
 
-get_next_position <- function(y, x, direction, dim_y, dim_x) {
-    
-    if (direction == "up") {
-        return(list(y = (y - 2) %% dim_y + 1, x = x, direction = "up"))
-    } else if (direction == "down") {
-        return(list(y = y %% dim_y + 1, x = x, direction = "down"))
-    }  else if (direction == "left") {
-        return(list(y = y, x = (x - 2) %% dim_x + 1, direction = "left"))
-    } else if (direction == "right") {
-        return(list(y = y, x = x %% dim_x + 1, direction = "right"))
-    }
-    
-    stop("Il y a une erreur !")
-}
-
-get_next_blizzard_position <- function(blizzard_position, dim_y, dim_x) {
-    return(purrr::pmap_df(blizzard_position, .f = get_next_position,
-                          dim_y = dim_y, dim_x = dim_x))
-}
-
 get_blizzard_map <- function(data_valley) {
+    
     blizzard_position <- get_blizzard_position(data_valley)
     
     dim_y <- length(data_valley) - 2
     dim_x <- nchar(data_valley[1]) - 2
     
     max_step <- ppcm(dim_y, dim_x)
-    mat <- array(FALSE, dim = c(dim_y, dim_x, max_step))
+    map <- array(FALSE, dim = c(dim_y, dim_x, max_step))
     
-    for (step in 1:max_step) {
-        blizzard_position <- get_next_blizzard_position(blizzard_position = blizzard_position, dim_y, dim_x)
-        for (blizzard in 1:nrow(blizzard_position)) {
-            mat[blizzard_position$y[blizzard], blizzard_position$x[blizzard], step] <- TRUE
+    for (blizzard in seq_len(nrow(blizzard_position))) {
+        
+        y <- blizzard_position$y[blizzard]
+        x <- blizzard_position$x[blizzard]
+        direction <- blizzard_position$direction[blizzard]
+        
+        if (direction == "up") {
+            map[, x, ] <- map[, x, ] | 
+                modulo_vectorise(seq_len(dim_y), pos = y, 
+                                 all_step = -(seq_len(max_step)))
+        } else if (direction == "down") {
+            map[, x, ] <- map[, x, ] | 
+                modulo_vectorise(seq_len(dim_y), pos = y, 
+                                 all_step = seq_len(max_step))
+        } else if (direction == "left") {
+            map[y, , ] <- map[y, , ] | 
+                modulo_vectorise(seq_len(dim_x), pos = x, 
+                                 all_step = -(seq_len(max_step)))
+        } else if (direction == "right") {
+            map[y, , ] <- map[y, , ] | 
+                modulo_vectorise(seq_len(dim_x), pos = x, 
+                                 all_step = seq_len(max_step))
         }
     }
     
-    return(mat)
+    return(map)
 }
 
 insert <- function(elt, listo) {
@@ -95,116 +101,128 @@ insert <- function(elt, listo) {
     return(c(listo[seq_len(index - 1)], list(elt), listo[index:length(listo)]))
 }
 
-search_path <- function(blizzard_map, dim_y, dim_x, start, end, step) {
+search_path <- function(blizzard_map, start, end, step) {
     
-    max_step <- ppcm(dim_y, dim_x)
+    dim_y <- dim(blizzard_map)[1]
+    dim_x <- dim(blizzard_map)[2]
+    max_step <- dim(blizzard_map)[3]
     
-    list_etat <- list(c(
-        y = start[1], x = start[2],
-        step = step,
-        dist = abs(1 - end[2]) + abs(1 - end[1]))
-    )
+    list_etat <- list(
+        list(c(
+            y = start[1], 
+            x = start[2],
+            step = step,
+            dist = abs(1 - end[2]) + abs(1 - end[1]))
+        ), list())
     
-    rec <- Inf
-    
-    while (length(list_etat) > 0) {
+    while (length(list_etat[[1]]) + length(list_etat[[2]]) > 0) {
         
-        etat <- list_etat[[1]]
-        list_etat <- list_etat[-1]
+        if (length(list_etat[[1]]) == 0) {
+            etat <- list_etat[[2]][[1]]
+            list_etat <- list(list_etat[[2]][-1], list())
+        } else {
+            etat <- list_etat[[1]][[1]]
+            list_etat[[1]] <- list_etat[[1]][-1]
+        }
         
         y <- etat[1]
         x <- etat[2]
         step <- etat[3]
         
-        if (step <= rec) {
+        
+        if (y == end[1] && x == end[2]) {
+            return(step + 1)
+        }
+        
+        if (y == 0) {
             
-            if (y == end[1] && x == end[2]) {
-                print(step)
-                rec <- step
+            if (!blizzard_map[y + 1, x, (step %% max_step) + 1]) {
+                list_etat[[2]] <- insert(listo = list_etat[[2]],
+                                         elt = c(y + 1, x, step + 1,
+                                                 abs(x - end[2]) + abs(y + 1 - end[1])
+                                         ))
+                blizzard_map[y + 1, x, (step %% max_step) + 1] <- TRUE
             }
             
-            if (y == 0) {
+            list_etat[[2]] <- insert(listo = list_etat[[2]],
+                                     elt = c(y, x, step + 1,
+                                             abs(x - end[2]) + abs(y - end[1])
+                                     ))
+            
+        } else if (y == dim_y + 1) {
+            
+            if (!blizzard_map[y - 1, x, (step %% max_step) + 1]) {
+                list_etat[[2]] <- insert(listo = list_etat[[2]],
+                                         elt = c(y - 1, x, step + 1,
+                                                 abs(x - end[2]) + abs(y - 1 - end[1])
+                                         ))
+                blizzard_map[y - 1, x, (step %% max_step) + 1] <- TRUE
+            }
+            
+            list_etat[[2]] <- insert(listo = list_etat[[2]],
+                                     elt = c(y, x, step + 1,
+                                             abs(x - end[2]) + abs(y - end[1])
+                                     ))
+        } else {
+            
+            if (y > 1 && !blizzard_map[y - 1, x, (step %% max_step) + 1]) {
+                list_etat[[2]] <- insert(listo = list_etat[[2]],
+                                         elt = c(y - 1, x, step + 1,
+                                                 abs(x - end[2]) + abs(y - 1 - end[1])
+                                         ))
                 
-                if (!blizzard_map[y + 1, x, (step  %% max_step) + 1]) {
-                    list_etat <- insert(listo = list_etat,
-                                        elt = c(y + 1, x, step + 1,
-                                                abs(x - end[2]) + abs(y + 1 - end[1])
-                                        ))
-                }
+                blizzard_map[y - 1, x, (step %% max_step) + 1] <- TRUE
+            }
+            
+            if (x > 1 && !blizzard_map[y, x - 1, (step %% max_step) + 1]) {
+                list_etat[[2]] <- insert(listo = list_etat[[2]],
+                                         elt = c(y, x - 1, step + 1,
+                                                 abs(x - 1 - end[2]) + abs(y - end[1])
+                                         ))
                 
-                list_etat <- insert(listo = list_etat,
-                                    elt = c(y, x, step + 1,
-                                            abs(x - end[2]) + abs(y - end[1])
-                                    ))
+                blizzard_map[y, x - 1, (step %% max_step) + 1] <- TRUE
+            }
+            
+            if (y < dim_y && !blizzard_map[y + 1, x, (step %% max_step) + 1]) {
+                list_etat[[2]] <- insert(listo = list_etat[[2]],
+                                         elt = c(y + 1, x, step + 1,
+                                                 abs(x - end[2]) + abs(y + 1 - end[1])
+                                         ))
                 
-            } else if (y == dim_y + 1) {
+                blizzard_map[y + 1, x, (step %% max_step) + 1] <- TRUE
+            }
+            
+            if (x < dim_x && !blizzard_map[y, x + 1, (step %% max_step) + 1]) {
+                list_etat[[2]] <- insert(listo = list_etat[[2]],
+                                         elt = c(y, x + 1, step + 1,
+                                                 abs(x + 1 - end[2]) + abs(y - end[1])
+                                         ))
                 
-                if (!blizzard_map[y - 1, x, (step  %% max_step) + 1]) {
-                    list_etat <- insert(listo = list_etat,
-                                        elt = c(y - 1, x, step + 1,
-                                                abs(x - end[2]) + abs(y - 1 - end[1])
-                                        ))
-                }
+                blizzard_map[y, x + 1, (step %% max_step) + 1] <- TRUE
+            }
+            
+            if (!blizzard_map[y, x, (step %% max_step) + 1]) {
+                list_etat[[2]] <- insert(listo = list_etat[[2]],
+                                         elt = c(y, x, step + 1,
+                                                 abs(x - end[2]) + abs(y - end[1])
+                                         ))
                 
-                list_etat <- insert(listo = list_etat,
-                                    elt = c(y, x, step + 1,
-                                            abs(x - end[2]) + abs(y - end[1])
-                                    ))
-                
-            } else {
-                
-                blizzard_map[y, x, ((step - 1) %% max_step) + 1] <- TRUE
-                
-                if (y > 1 && !blizzard_map[y - 1, x, (step  %% max_step) + 1]) {
-                    list_etat <- insert(listo = list_etat,
-                                        elt = c(y - 1, x, step + 1,
-                                                abs(x - end[2]) + abs(y - 1 - end[1])
-                                        ))
-                }
-                
-                if (x > 1 && !blizzard_map[y, x - 1, (step  %% max_step) + 1]) {
-                    list_etat <- insert(listo = list_etat,
-                                        elt = c(y, x - 1, step + 1,
-                                                abs(x - 1 - end[2]) + abs(y - end[1])
-                                        ))
-                }
-                
-                if (y < dim_y && !blizzard_map[y + 1, x, (step  %% max_step) + 1]) {
-                    list_etat <- insert(listo = list_etat,
-                                        elt = c(y + 1, x, step + 1,
-                                                abs(x - end[2]) + abs(y + 1 - end[1])
-                                        ))
-                }
-                
-                if (x < dim_x && !blizzard_map[y, x + 1, (step  %% max_step) + 1]) {
-                    list_etat <- insert(listo = list_etat,
-                                        elt = c(y, x + 1, step + 1,
-                                                abs(x + 1 - end[2]) + abs(y - end[1])
-                                        ))
-                }
-                
-                if (!blizzard_map[y, x, (step  %% max_step) + 1]) {
-                    list_etat <- insert(listo = list_etat,
-                                        elt = c(y, x, step + 1,
-                                                abs(x - end[2]) + abs(y - end[1])
-                                        ))
-                }
+                blizzard_map[y, x, (step %% max_step) + 1] <- TRUE
             }
         }
     }
     
-    return(rec + 1)
+    stop("Il n'y a pas de solutions.")
 }
 
 solve_day24_part1 <- function(data_valley) {
     
     blizzard_map <- get_blizzard_map(data_valley)
     
-    dim_y <- length(data_valley) - 2
-    dim_x <- nchar(data_valley[1]) - 2
+    dim_y <- dim(blizzard_map)[1]
+    dim_x <- dim(blizzard_map)[2]
     
     return(search_path(blizzard_map = blizzard_map, 
-                       dim_y = dim_y, dim_x = dim_x, 
                        start = c(0, 1), end = c(dim_y, dim_x), step = 0))
 }
 
@@ -212,17 +230,14 @@ solve_day24_part2 <- function(data_valley) {
     
     blizzard_map <- get_blizzard_map(data_valley)
     
-    dim_y <- length(data_valley) - 2
-    dim_x <- nchar(data_valley[1]) - 2
+    dim_y <- dim(blizzard_map)[1]
+    dim_x <- dim(blizzard_map)[2]
     
     step <- search_path(blizzard_map = blizzard_map, 
-                        dim_y = dim_y, dim_x = dim_x, 
                         start = c(0, 1), end = c(dim_y, dim_x), step = 0)
     step <- search_path(blizzard_map = blizzard_map, 
-                        dim_y = dim_y, dim_x = dim_x, 
                         start = c(dim_y + 1, dim_x), end = c(1, 1), step = step)
     return(search_path(blizzard_map = blizzard_map, 
-                       dim_y = dim_y, dim_x = dim_x, 
                        start = c(0, 1), end = c(dim_y, dim_x), step = step))
 }
 
