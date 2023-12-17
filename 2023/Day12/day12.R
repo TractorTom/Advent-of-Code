60bee0a89368f5564c7f9d9de9e57cc6f897c531
Tanguy BARTHELEMY <tangbarth@hotmail.fr>
2023-12-17 19:14:50 +0100

    update day 12

--- 2023/Day12/day12.R
+++ 2023/Day12/day12.R
@@ -15,6 +15,20 @@ unknown_spring_example <- readLines(
 
 
 # Déclaration fonction ---------------------------------------------------------
+# je met +1 pour le 0 (même si on sera obligé de faire index + 1 car pas d'index 0 en R)
+fun_fact <- function(dx, dy, dz) {
+    state_array <- array(data = 0,
+                   dim = c(force(dx) + 1, force(dy) + 1, force(dz) + 1))
+
+    function(x, y, z, value = NULL) {
+        if (!missing(x)) x <- x + 1
+        if (!missing(y)) y <- y + 1
+        if (!missing(z)) z <- z + 1
+
+        if (is.null(value)) return(state_array[x, y, z])
+        state_array[x, y, z] <<- value + state_array[x, y, z]
+    }
+}
 
 count_arrangements <- function(spring, contiguous_group) {
 
@@ -26,10 +40,11 @@ count_arrangements <- function(spring, contiguous_group) {
     #   - len : le nombre de # que l'on a en réserve
 
     # je met +1 pour le 0 (même si on sera obligé de faire index + 1 car pas d'index 0 en R)
-    state <- array(data = 0,
-                   dim = c(nchar(spring) + 1,
-                           length(contiguous_group) + 1,
-                           max(contiguous_group) + 1))
+    state <- fun_fact(
+        nchar(spring),
+        length(contiguous_group),
+        max(contiguous_group)
+    )
 
     # Maintenant il faut écrire les fonctions de passage
     # Si spring[pos] = # --> alors len devient len + 1 et state[pos + 1, groups, len + 1] = state[pos + 1, groups, len]
@@ -46,43 +61,31 @@ count_arrangements <- function(spring, contiguous_group) {
     # A chaque fois, le sinon = 0
     # chaque fois qu'on fait appel à contiguous_group[group + 1] c'est pour group + 1 <= length(contiguous_group)
 
-    # Le fonction suivantes sont très intéressantes mais malheureusement trop lentes... donc inutilisées
-    f <- function(state, x, y, z) {
-        return(state[x + 1, y + 1, z + 1])
-    }
 
-    `f<-` <- function(state, x, y, z, value) {
-        state[x + 1, y + 1, z + 1] <- value + state[x + 1, y + 1, z + 1]
-        return(state)
-    }
-    # Donc on ajoute +1 partout pour coller avec les indices de l'array
-
-    state[0 +1, 0 +1, 0 +1] <- 1
+    state(0, 0, 0, value = 1)
 
     for (pos in 1:(nchar(spring))) {
         val <- substr(spring, pos, pos)
 
         if (val %in% c(".", "?")) {
-            state[pos +1, , 0 +1] <- state[pos - 1 +1, , 0 +1]
+            state(pos, , 0, value = state(pos - 1, , 0))
         }
-        for (groups in 0:length(contiguous_group)) {
+        for (groups in seq_along(contiguous_group)) {
+            nb_group <- contiguous_group[groups]
 
-            if (val %in% c(".", "?") && groups > 0) {
-                state[pos +1, groups +1, 0 +1] <-
-                    state[pos +1, groups +1, 0 +1] +
-                    state[pos - 1 +1, groups - 1 +1, contiguous_group[groups] +1]
+            if (val %in% c("#", "?")) {
+                state(pos, groups - 1, seq_len(nb_group),
+                      value = state(pos - 1, groups - 1, seq_len(nb_group) - 1))
             }
 
-            if (val %in% c("#", "?") && groups < length(contiguous_group)) {
-                for (len in seq_len(contiguous_group[groups + 1]) - 1) {
-                    state[pos +1, groups +1, len + 1 +1] <-
-                        state[pos - 1 +1, groups +1, len +1]
-                }
+            if (val %in% c(".", "?")) {
+                state(pos, groups, 0,
+                      value = state(pos - 1, groups - 1, nb_group))
             }
         }
     }
 
-    return(state[nchar(spring) +1, length(contiguous_group) +1, 0 +1])
+    return(state(nchar(spring), length(contiguous_group), 0))
 }
 
 solve_day12_part1 <- function(data_spring) {
