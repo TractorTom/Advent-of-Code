9641c687a5fac07457cd81430e9f328bfc940f59
Tanguy BARTHELEMY <tangbarth@hotmail.fr>
2024-12-05 15:03:50 +0100

    update day05

---
+++ 2024/Day05/day05.R
@@ -0,0 +1,99 @@
+# Titre : day05.R
+# Auteur : Tanguy
+
+##############
+##  DAY 05  ##
+##############
+
+# proffer::pprof({
+# Import data ------------------------------------------------------------------
+
+manual <- readLines(file.path("2024", "Day05", "manual.txt"))
+manual_example <- readLines(file.path("2024", "Day05", "manual_example.txt"))
+
+
+# Déclaration fonction ---------------------------------------------------------
+
+pre_treatment <- function(data_manual) {
+    sep <- which(data_manual == "")
+    rules <- data_manual[seq_len(sep - 1L)] |>
+        strsplit(split = "|", fixed = TRUE) |>
+        do.call(what = rbind) |>
+        as.data.frame()
+    updates <- data_manual[(sep + 1L):(length(data_manual))]
+
+    return(list(rules = rules, updates = updates))
+}
+
+is_sorted <- function(x, rules) {
+    positions <- seq_along(x)
+    names(positions) <- x
+    condition <- all(positions[rules[["V1"]]] < positions[rules[["V2"]]])
+    return(condition)
+}
+
+solve_day05_part1 <- function(data_manual) {
+
+    l_manual <- pre_treatment(data_manual)
+    rules <- l_manual[["rules"]]
+    updates <- l_manual[["updates"]]
+
+    output <- 0L
+    for (u in updates) {
+        pages <- strsplit(u, split = ",", fixed = TRUE) |>
+            unlist() |>
+            as.numeric()
+        rrules <- rules[rules[["V1"]] %in% pages
+                        & rules[["V2"]] %in% pages, ]
+        if (is_sorted(pages, rrules)) {
+            output <-  output + pages[(length(pages) + 1L) / 2L]
+        }
+    }
+    return(output)
+}
+
+rearrange <- function(x, rules) {
+    positions <- seq_along(x)
+    names(positions) <- x
+    while (length(x) > 1L) {
+        never_first <- setdiff(x, rules[["V1"]])
+        never_second <- setdiff(x, rules[["V2"]])
+        rules <- rules[rules[["V1"]] != never_second
+                       & rules[["V2"]] != never_first, ]
+        x <- x[!x %in% c(never_first, never_second)]
+    }
+    return(x)
+}
+
+solve_day05_part2 <- function(data_manual) {
+    l_manual <- pre_treatment(data_manual)
+    rules <- l_manual[["rules"]]
+    updates <- l_manual[["updates"]]
+
+    output <- 0L
+    for (u in updates) {
+        pages <- strsplit(u, split = ",", fixed = TRUE) |>
+            unlist() |>
+            as.numeric()
+        rrules <- rules[rules[["V1"]] %in% pages
+                        & rules[["V2"]] %in% pages, ]
+        if (!is_sorted(pages, rrules)) {
+            output <- output + rearrange(pages, rrules)
+        }
+    }
+    return(output)
+}
+
+
+# Execution --------------------------------------------------------------------
+
+## Part 1 ----------------------------------------------------------------------
+
+solve_day05_part1(manual_example)
+solve_day05_part1(manual)
+
+
+## Part 2 ----------------------------------------------------------------------
+
+solve_day05_part2(manual_example)
+solve_day05_part2(manual)
