82863fe0ebc833c0869f4b7ec61087fa03f95701
Tanguy Barthelemy <tangbarth@hotmail.fr>
2024-12-15 19:20:02 +0100

    update day13

--- 2024/Day13/day13.R
+++ 2024/Day13/day13.R
@@ -10,46 +10,54 @@
 
 # Import data ------------------------------------------------------------------
 
-claws <- read.table(file.path("2024", "Day13", "claws.txt"))
-claws_example <- read.table(file.path("2024", "Day13", "claws_example.txt"))
+claws <- readLines(file.path("2024", "Day13", "claws.txt"))
+claws_example <- readLines(file.path("2024", "Day13", "claws_example.txt"))
 
 
 # Déclaration fonction ---------------------------------------------------------
 
-# Résolution du système
-# u * xa + v + xb = x
-# u * ya + v + yb = y
-solve_equation <- function(xa, xb, x, ya, yb, y) {
+source("R/utils.R")
 
-    v <- (x * ya - y * xa) %/% (xb * ya - yb * xa)
-    u <- (x - v * xb) %/% xa
+read_claws_data <- function(input_claws) {
+    pattern <- "^(?:Button [AB]|Prize): X[+=](\\d+), Y[+=](\\d+)$"
+    proto <- list(X = integer(), Y = integer())
+    equations <- input_claws[input_claws != ""] |>
+        extract_regex(pattern, proto) |>
+        as.matrix()
+    return(equations)
+}
 
-    if ((x * ya - y * xa) %% (xb * ya - yb * xa) == 0L
-        && (x - v * xb) %% xa == 0L) {
-        return(c(u, v))
-    }
-    return(NULL)
+solve_equation <- function(mat) {
+    if (det(mat) == 0L) return(NULL)
+    return((solve(mat) * det(mat)) |> round())
 }
 
 count_token <- function(data_claw, offset = 0L) {
     tokens <- 0L
     for (id_claw in (seq_len(nrow(data_claw) %/% 3L) - 1L)) {
-        equations <- as.list(c(data_claw[1L:3L + 3L * id_claw, 1L],
-                               data_claw[1L:3L + 3L * id_claw, 2L]) +
-                                 c(0L, 0L, offset, 0L, 0L, offset))
-        new_tokens <- do.call(solve_equation, equations)
-        tokens <- tokens + sum(c(3L, 1L) * new_tokens)
+        A <- t(data_claw[3L * id_claw + 1L:2L, ])
+        Y <- offset + data_claw[3L * id_claw + 3L, ]
+        A_1 <- solve_equation(A)
+        d <- round(det(A))
+        if (!is.null(A_1)) {
+            new_tokens <- c(3L, 1L) %*% A_1 %*% Y
+            if (new_tokens %% d == 0L) {
+                tokens <- tokens + round(new_tokens %/% d)
+            }
+        }
     }
     return(tokens)
 }
 
-solve_day13_part1 <- function(data_claw) {
+solve_day13_part1 <- function(input_claws) {
+    data_claw <- read_claws_data(input_claws)
     tokens <- count_token(data_claw)
     return(tokens)
 }
 
-solve_day13_part2 <- function(data_claw) {
-    tokens <- count_token(data_claw, offset = 10000000000000)
+solve_day13_part2 <- function(input_claws) {
+    data_claw <- read_claws_data(input_claws)
+    tokens <- count_token(data_claw, offset = 10000000000000.0)
     return(tokens)
 }
 
