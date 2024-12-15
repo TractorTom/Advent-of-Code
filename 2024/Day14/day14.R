31a33da31e1a7635351be9477325046e21fea977
Tanguy Barthelemy <tangbarth@hotmail.fr>
2024-12-15 19:42:18 +0100

    update day14

--- 2024/Day14/day14.R
+++ 2024/Day14/day14.R
@@ -10,22 +10,24 @@
 
 # Import data ------------------------------------------------------------------
 
-robots <- readLines(file.path("2024", "Day14", "data.txt"))
-robots_example <- readLines(file.path("2024", "Day14", "data_example.txt"))
+robots <- readLines(file.path("2024", "Day14", "robots.txt"))
+robots_example <- readLines(file.path("2024", "Day14", "robots_example.txt"))
 
 
 # Déclaration fonction ---------------------------------------------------------
 
-pre_treatment <- function(input_robots) {
-    data_robots <- input_robots |>
-        strsplit(split = " ", fixed = TRUE) |>
-        lapply(substr, start = 3L, stop = 50L) |>
-        lapply(\(x) x |> strsplit(split = ",", fixed = TRUE) |> unlist() |> as.numeric()) |>
-        do.call(what = rbind) |>
-        as.data.frame()
-    data_robots[["x"]] <- data_robots[["V1"]] + 1L
-    data_robots[["y"]] <- data_robots[["V2"]] + 1L
+source("R/utils.R")
 
+read_robots_data <- function(input_robots) {
+    pattern <- "^p=(-?\\d+),(-?\\d+) v=(-?\\d+),(-?\\d+)$"
+    proto <- list(x_init = integer(),
+                  y_init = integer(),
+                  vx = integer(),
+                  vy = integer())
+    data_robots <- input_robots[input_robots != ""] |>
+        extract_regex(pattern, proto)
+    data_robots[["x"]] <- data_robots[["x_init"]] + 1L
+    data_robots[["y"]] <- data_robots[["y_init"]] + 1L
     return(data_robots)
 }
 
@@ -37,16 +39,16 @@ visualize <- function(data_robots, dimensions) {
     return(map)
 }
 
-update_position <- function(data_robots, dimensions, seconds) {
-    data_robots[["x"]] <- (data_robots[["V1"]] + data_robots[["V3"]] * seconds) %% dimensions[[1L]] + 1L
-    data_robots[["y"]] <- (data_robots[["V2"]] + data_robots[["V4"]] * seconds) %% dimensions[[2L]] + 1L
+teleport <- function(data_robots, dimensions, seconds) {
+    data_robots[["x"]] <- (data_robots[["x_init"]] + data_robots[["vx"]] * seconds) %% dimensions[[1L]] + 1L
+    data_robots[["y"]] <- (data_robots[["y_init"]] + data_robots[["vy"]] * seconds) %% dimensions[[2L]] + 1L
 
     return(data_robots)
 }
 
 solve_day14_part1 <- function(input_robots, dimensions) {
-    data_robots <- pre_treatment(input_robots) |>
-        update_position(dimensions, 100L)
+    data_robots <- read_robots_data(input_robots) |>
+        teleport(dimensions, 100L)
 
     safety_factor <- prod(
         sum(data_robots[["x"]] <= dimensions[[1L]] %/% 2L
@@ -64,12 +66,12 @@ solve_day14_part1 <- function(input_robots, dimensions) {
 
 solve_day14_part2 <- function(input_robots, dimensions) {
 
-    data_robots <- pre_treatment(input_robots)
+    data_robots <- read_robots_data(input_robots)
 
     all_density <- NULL
     for (seconds in seq_len(prod(dimensions))) {
         data_robots <- data_robots |>
-            update_position(dimensions, seconds)
+            teleport(dimensions, seconds)
         density_seconds <- (sum(data_robots[["y"]] + 2L * data_robots[["x"]] <= 100L) +
                                 sum(data_robots[["y"]] - 2L * data_robots[["x"]] <= -100L)) / nrow(data_robots)
         all_density <- c(all_density, density_seconds)
@@ -82,10 +84,11 @@ solve_day14_part2 <- function(input_robots, dimensions) {
     while (!cond) {
         seconds <- as.integer(names(all_density)[k])
         data_robots |>
-            update_position(dimensions, seconds) |>
+            teleport(dimensions, seconds) |>
             visualize(dimensions) |>
             apply(MARGIN = 1L, FUN = paste0, collapse = "") |>
             cat(sep = "\n")
+        print(seconds)
         cond <- tolower(readline()) %in% c("yes", "y", "ok", "o", "oui")
         k <- k + 1L
     }
@@ -98,7 +101,7 @@ solve_day14_part2 <- function(input_robots, dimensions) {
 
 ## Part 1 ----------------------------------------------------------------------
 
-solve_day14_part1(robots_example, c(7L, 11L))
+solve_day14_part1(robots_example, c(11L, 7L))
 solve_day14_part1(robots, c(101L, 103L))
 
 
